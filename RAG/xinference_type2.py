99ed17765cd1db7309f86ecb5a52feee071d8619
changz1115 <8434409@qq.com>
2024-03-26 17:48:40 +0800

    modified:   RAG/xinference_type2.py

--- RAG/xinference_type2.py
+++ RAG/xinference_type2.py
@@ -1,30 +1,35 @@
-from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings.xinference import XinferenceEmbeddings
 from langchain_community.llms import Xinference
+from langchain_community.embeddings.xinference import XinferenceEmbeddings
+
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import DocArrayInMemorySearch
+
+from langchain.text_splitter import CharacterTextSplitter
+
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.vectorstores import DocArrayInMemorySearch
 
 def main():
+    print("主模型的使用")
     model_local = Xinference(server_url="http://10.1.104.172:9997", model_uid="chatglm2")
 
     print("1. 读取文件并分词")
     documents = TextLoader("./三国演义白话文完整版.txt").load()
+    print("1.1 分词")
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
     doc_splits = text_splitter.split_documents(documents)
 
-    print("2. 嵌入并存储")
-    print("2.1")
+    print("2. 矢量并存储")
+    print("2.1 矢量模型的使用")
     embeddings = XinferenceEmbeddings(server_url="http://10.1.104.172:9997", model_uid="text2vec-base-chinese-sentence")
-    print("2.2 vectorstore 矢量存储memory DocArrayInMemorySearch")
+    print("2.2 vectorstore 矢量數據存储在memory DocArrayInMemorySearch")
     vectorstore = DocArrayInMemorySearch.from_documents(doc_splits, embeddings)
     print("2.3")
     retriever = vectorstore.as_retriever()
     print(retriever)
 
-    print("3. 向模型提问")
+    print("3. 向主模型提问 定義格式")
     template = """Answer the question based only on the following context:
     {context}
     Question: {question}
